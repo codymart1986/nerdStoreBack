dfdb0059c6c9f3480097308c3f4434e5e4c0634f
codymart1986 <codymart1986@hotmail.com>
2021-12-20 10:06:05 -0800

    creating schemas

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const connectDB = require("./startup/db");
 const mongoose = require("mongoose");
+const user = require("./routes/user")
 
 const app = express();
 
 connectDB();
+
+app.use(cors())
+app.use(express.json());
+app.use("/api/users", user);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
