79af3216b49885b09913339e3b5144154a5f7eb5
codymart1986 <codymart1986@hotmail.com>
2021-12-20 14:35:11 -0800

    updated user model and implemented JWT, register and login working

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require("express");
-const connectDB = require("./startup/db");
-const mongoose = require("mongoose");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./startup/db');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const mongoose = require('mongoose')
+const fs = require('fs');
+require('dotenv/config');
 
 const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
-app.use("/api/users", users);
-app.use("/api/auth", auth);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log(`Server started on port: ${port}`)
-})
+  console.log(`Server started on port: ${port}`);
+});
+
