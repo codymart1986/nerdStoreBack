77b90d74d7793a4d4449324661b4c36f71bb4a87
codymart1986 <codymart1986@hotmail.com>
2021-12-20 09:27:10 -0800

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const connectDB = require("./startup/db");
+const mongoose = require("mongoose");
+
 const app = express();
 
+connectDB();
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+})
